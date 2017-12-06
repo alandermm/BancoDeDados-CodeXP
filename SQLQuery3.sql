564b96bc59b236f6aa37d3f02817c50295a113f7
alandermm <alandermm@hotmail.com>
2017-12-06 21:47:52 -0200

    Create SQLQuery3.sql

---
+++ SQLQuery3.sql
@@ -0,0 +1,11 @@
+/*insert into clientes(nomeCliente, email, cpf) values('Pedro', 'pedro@uol.com.br', '1212211212')*/
+
+/*CREATE PROCEDURE sp_CadCliente
+@nome varchar(50),
+@email varchar(100),
+@cpf varchar(20)
+
+as
+insert into Clientes(nome, email, cpf) values(@nome, @email, @cpf)*/
+
+--exec sp_CadCliente 'Maria', 'maria@uol.com.br', '0202020202'
